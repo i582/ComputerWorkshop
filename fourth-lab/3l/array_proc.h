d2a22f900b4233a952f0a4936f91f166a732851f
i582 <i58215@voenmeh.ru>
2019-10-11 21:22:07 +0300

    update

--- fourth-lab/3l/array_proc.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-extern "C" __declspec(dllexport) int array_proc(int* mas, int count);
--- fourth-lab/3l/matrix_proc.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-extern "C" __declspec(dllexport) int matrix_proc(int** matrix, int i, int j);
